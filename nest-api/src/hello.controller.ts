ea8313257d70894248821826584580d4a0770cd7
ohhuynji <helloqu@naver.com>
2025-03-25 11:37:03 +0900

    백엔드 프론트엔드 도메인 같은경로로 연결

--- nest-api/src/hello.controller.ts
+++ nest-api/src/hello.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { HelloService } from './hello.service';
 
-@Controller('api/hello')
+@Controller('hello')
 export class HelloController {
   constructor(private readonly helloService: HelloService) {}
 
