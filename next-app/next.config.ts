2b4adad2e1b739e0809ed4cfb116d7fd8ec9c364
ohhuynji <helloqu@naver.com>
2025-03-25 10:16:11 +0900

    글로벌 프리픽스

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -1,17 +1,14 @@
-import type { NextConfig } from "next";
-
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
   output: 'export',
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://46bemqujkp.us-east-1.awsapprunner.com/api/:path*', // App Runner의 API URL
+        destination: 'https://46bemqujkp.us-east-1.awsapprunner.com/api/:path*', // App Runner의 API URL로 변경
       },
     ];
   },
 };
-
 
 export default nextConfig;
