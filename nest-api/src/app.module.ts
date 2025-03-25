74b264fec29677870ae331cfb06491de4931452c
ohhuynji <helloqu@naver.com>
2025-03-25 15:20:30 +0900

    apprunner 버전 끝끝

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloController } from './hello.controller';
 import { HelloService } from './hello.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController, HelloController], // HelloController 추가
-  providers: [AppService, HelloService], // HelloService 추가
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // 전역으로 환경 변수 사용
+    PrismaModule, // PrismaModule을 추가하여 PrismaService를 전역으로 사용
+  ],
+  controllers: [AppController, HelloController],
+  providers: [AppService, HelloService],
 })
 export class AppModule {}
