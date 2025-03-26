d3517dfd7ecd6929b96bd68b1b810afc2b7e896b
ohhuynji <helloqu@naver.com>
2025-03-26 16:48:11 +0900

    수정완

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -1,11 +1,12 @@
 // next.config.js
 const nextConfig = {
+  output: 'export',
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://46bemqujkp.us-east-1.awsapprunner.com/api/:path*', // App Runner의 API URL로 변경
-        // destination: 'http://infrapj-alb-975883155.us-east-1.elb.amazonaws.com/api/:path*', // App Runner의 API URL로 변경
+        // destination: 'https://46bemqujkp.us-east-1.awsapprunner.com/api/:path*', // App Runner의 API URL로 변경
+        destination: 'http://infrapj-alb-975883155.us-east-1.elb.amazonaws.com/api/:path*', // App Runner의 API URL로 변경
 
         
       },
