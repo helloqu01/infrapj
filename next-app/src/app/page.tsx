a95e616f93c1368efdc115067488c8eb75c0475f
ohhuynji <helloqu@naver.com>
2025-04-03 17:35:53 +0900

    수정

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,10 +1,19 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import About from "@/components/About";
+import Footer from "@/components/Footer";
+import ThemeToggle from "@/components/ThemeToggle";
 
-
-export default function Home() { 
+export default function Home() {
   return (
     <>
-  
-      test
+      <Navbar />
+      <Hero />
+      <Services />
+      <About />
+      <Footer />
+      <ThemeToggle />
     </>
   );
 }
