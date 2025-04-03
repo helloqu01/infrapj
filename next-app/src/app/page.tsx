a0a4d5066819876dca30529a3a2119824248824e
ohhuynji <helloqu@naver.com>
2025-04-03 12:18:06 +0900

    / 페이지 404 오류 수정

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,3 +1,6 @@
+// src/app/page.tsx
+export const dynamic = 'force-dynamic';
+
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
